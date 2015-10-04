2df41c54341472348eb2b9d2f86578dba1d423fd
Sig Lange <sig.lange@gmail.com>
2015-10-04 15:04:37 -0700

    add support for sided dice

--- cmd/dice/main.go
+++ cmd/dice/main.go
@@ -13,16 +13,37 @@ type Options struct {
 
 	// number of dice to roll
 	NumDice int
+
+	// sides of a die
+	Sides int
 }
 
-type Die int
+type Die struct {
+	Sides int
+	Value int
+}
+
+func NewDie() *Die {
+	return &Die{
+		Sides: 6,
+		Value: 0,
+	}
+}
+
+func NewSidedDie(sides int) *Die {
+	d := NewDie()
+	d.Sides = sides
+	return d
+}
 
 func (d *Die) Roll() {
-	v := (rand.Int() % 6) + 1
-	*d = Die(v)
+	v := (rand.Int() % d.Sides) + 1
+	d.Value = v
 }
-func NewRoll() *Die {
-	d := new(Die)
+
+// make a new die and roll it
+func NewRoll(sides int) *Die {
+	d := NewSidedDie(sides)
 	d.Roll()
 	return d
 }
@@ -37,7 +58,7 @@ func (dice Dice) Roll() {
 func (dice Dice) String() string {
 	v := make([]int, len(dice))
 	for i, d := range dice {
-		v[i] = int(*d)
+		v[i] = d.Value
 	}
 	return fmt.Sprintf("%v", v)
 }
@@ -50,18 +71,20 @@ func main() {
 	opts := &Options{
 		Rolls:   1,
 		NumDice: 1,
+		Sides:   6,
 	}
 
 	flag.IntVar(&opts.NumDice, "num", opts.NumDice, "number of dice to roll")
 	flag.IntVar(&opts.NumDice, "n", opts.NumDice, "alias for num")
 	flag.IntVar(&opts.Rolls, "rolls", opts.Rolls, "number of rolls")
-	flag.IntVar(&opts.Rolls, "r", opts.Rolls, "alias for rolls")
+	flag.IntVar(&opts.Sides, "s", opts.Sides, "alias for sides")
+	flag.IntVar(&opts.Sides, "sides", opts.Sides, "alias for sides")
 	flag.Parse()
 
 	for roll := 1; roll <= opts.Rolls; roll++ {
 		dice := make(Dice, opts.NumDice)
 		for i := 0; i < opts.NumDice; i++ {
-			dice[i] = NewRoll()
+			dice[i] = NewRoll(opts.Sides)
 		}
 		fmt.Printf("roll %d %s\n", roll, dice)
 
